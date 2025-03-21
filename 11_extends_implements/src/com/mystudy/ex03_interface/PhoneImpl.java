bd679304c2e1f86303d300b3e1c98d57b2b00f4e
SohyeonSung <sasa547@naver.com>
2025-03-21 09:39:53 +0900

    update 0939

---
+++ 11_extends_implements/src/com/mystudy/ex03_interface/PhoneImpl.java
@@ -0,0 +1,40 @@
+package com.mystudy.ex03_interface;
+
+// 인터페이스를 구현(Implements)해서 클래스를 만들기
+class PhoneImpl implements I_Phone {
+
+	
+	//어노테이션 : 컴파일러에게 알려주기 위한 주석문
+	// @Override : 오버라이딩해서 만든 메소드 의미
+	
+	@Override 
+	public void view() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void call() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receiveCall() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendMsg() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receiveMsg() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
